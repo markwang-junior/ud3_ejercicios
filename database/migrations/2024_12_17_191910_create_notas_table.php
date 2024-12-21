400c1a89c6d569d4d2ceda55548b02f74771465f
Mark <mark.wg2001@gmail.com>
2024-12-21 12:03:06 +0100

    falta por comprobar ejercicio 10

--- database/migrations/2024_12_17_191910_create_notas_table.php
+++ database/migrations/2024_12_17_191910_create_notas_table.php
@@ -15,13 +15,19 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('alumno_id');
             $table->unsignedBigInteger('asignatura_id');
-            $table->decimal('nota', 5,2);
+            $table->decimal('nota', 5, 2);
             $table->timestamps();
-        
-            $table->foreign('alumno_id')->references('id')->on('alumnos')->onDelete('cascade');
-            $table->foreign('asignatura_id')->references('id')->on('asignaturas')->onDelete('cascade');
+
+            $table->foreign('alumno_id')
+                  ->references('id')
+                  ->on('alumnos')
+                  ->onDelete('cascade');
+
+            $table->foreign('asignatura_id')
+                  ->references('id')
+                  ->on('asignaturas')
+                  ->onDelete('cascade');
         });
-        
     }
 
     /**
